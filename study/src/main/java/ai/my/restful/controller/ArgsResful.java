d0981f10fc4173dab9a29f20d13ef198f45b467c
shanghang <9LxjeqhOxS5A>
2020-07-29 23:35:40 +0800

    @PostMapping声明一个post请求，@RequestBody获取post请求的报文体

---
+++ study/src/main/java/ai/my/restful/controller/ArgsResful.java
@@ -0,0 +1,23 @@
+package ai.my.restful.controller;
+
+import ai.my.restful.pojo.Author;
+import ai.my.restful.pojo.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: shanghang
+ * @Project:create-wheel
+ * @description:post请求
+ * @Date: 2020/7/29 23:22
+ **/
+@RestController
+@Slf4j
+public class ArgsResful {
+    @PostMapping("/setBook")
+    public void doAdd(@RequestBody String json){
+      log.error(json);
+    }
+}
---
+++ study/src/main/java/ai/my/restful/pojo/Author.java
@@ -0,0 +1,15 @@
+package ai.my.restful.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: shanghang
+ * @Project:create-wheel
+ * @description:Author
+ * @Date: 2020/7/29 23:24
+ **/
+@Data
+public class Author {
+    private String name;
+    private Integer age;
+}
