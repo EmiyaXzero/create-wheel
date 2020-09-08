0cfe31fd3adb8480b7850faf10295c2ebea8ac1c
shanghang <9LxjeqhOxS5A>
2020-09-08 23:55:48 +0800

    SkyWalking Demo

---
+++ study/demo-api/src/main/java/api/interfaces/ApiService.java
@@ -0,0 +1,11 @@
+package api.interfaces;
+
+/**
+ * @Author: shanghang
+ * @Project:create-wheel
+ * @description:依赖接口类
+ * @Date: 2020/9/8 23:50
+ **/
+public interface ApiService {
+    public String say(String name) throws Exception;
+}
